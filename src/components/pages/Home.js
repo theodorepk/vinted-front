61b281e06f8c2cabaaa096aced8296dcd373db33
theodorepk <theodore.perron.kyritsos@gmail.com>
2022-07-28 20:02:04 +0200

    offers on home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,7 @@
 const Home = ({ data }) => {
   data.offers.map((element) => {
     if (element.owner) {
-      return console.log(element.owner.account.username);
+      return console.log(element.product_image.secure_url);
     } else {
       return null;
     }
@@ -11,15 +11,31 @@ const Home = ({ data }) => {
   return (
     <div>
       <div className="hero"></div>
-      {data.offers.map((element, index) => {
-        return (
-          <div className="product" key={index}>
-            <span>{element.product_name}</span>
-            {/* <span>{element.owner.account.username}</span> */}
-            {element.owner && <span> {element.owner.account.username}</span>}
-          </div>
-        );
-      })}
+      <div className="homeOffers">
+        {data.offers.map((element, index) => {
+          return (
+            <div className="product" key={index}>
+              <span>{element.product_name}</span>
+              {/* some offers don't have owner */}
+              {element.owner && (
+                <div className="infoOwner">
+                  <img
+                    src={element.owner.account.avatar.secure_url}
+                    alt="avatar du vendeur"
+                  />
+                  <span> {element.owner.account.username}</span>
+                </div>
+              )}
+              {
+                <img
+                  src={element.product_image.secure_url}
+                  alt="produit vendu"
+                />
+              }
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
