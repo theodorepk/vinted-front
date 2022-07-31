69a736189ac8bab02bbc96014f0aac53a54f401a
theodorepk <theodore.perron.kyritsos@gmail.com>
2022-07-31 11:16:54 +0200

    clean dom

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
-import Home from "./components/pages/Home";
+import Home from "./pages/Home";
 import Header from "./components/Header";
-import Offer from "./components/pages/Offer";
-import SignUp from "./components/pages/Signup";
-import Login from "./components/Login";
+import Offer from "./pages/Offer";
+import SignUp from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
   const [userToken, setUserToken] = useState(``);
