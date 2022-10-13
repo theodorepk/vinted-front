51b7a5057712aa9ba911f070db88f89b48bdd593
theodorepk <theodore.perron.kyritsos@gmail.com>
2022-10-13 15:20:38 +0200

    home page css

--- src/components/MiniProduct.js
+++ src/components/MiniProduct.js
@@ -5,19 +5,20 @@ const MiniProduct = ({ product }) => {
       {product.owner && (
         <div className="infoOwner">
           {product.owner.account.avatar && (
-            <img
-              src={product.owner.account.avatar.secure_url}
-              alt="avatar du vendeur"
-            />
+            <>
+              <img
+                src={product.owner.account.avatar.secure_url}
+                alt="avatar du vendeur"
+              />
+              <span> {product.owner.account.username}</span>
+            </>
           )}
-
-          <span> {product.owner.account.username}</span>
         </div>
       )}
       {product.product_image.secure_url && (
         <img src={product.product_image.secure_url} alt="produit vendu" />
       )}
-      <div>
+      <div className="infoProduct">
         <div>
           <span>{product.product_price} €</span>
         </div>
