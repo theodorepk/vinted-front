753dcfa12fc250e2c273f711e519a33c4ec04de6
theodorepk <theodore.perron.kyritsos@gmail.com>
2022-12-02 15:11:14 +0100

    responsive payment

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,16 @@
 const SearchBar = ({ setTitle, title }) => {
   return (
+    // <div className="searchBar">
     <input
       key="searchBar"
       className="searchBar"
-      placeholder="🔍 rechercher des articles"
+      placeholder=" Rechercher des articles"
       value={title}
       onChange={(event) => {
         setTitle(event.target.value);
       }}
     />
+    // </div>
   );
 };
 
